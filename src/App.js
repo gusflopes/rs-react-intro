53995cf65769a40b56821b08787b2cc8e2698ef2
gusflopes <gusflopes86@gmail.com>
2019-10-22 14:41:57 -0400

    first Hello World component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './App.css';
+
+import profile from './assets/profile.png';
 
 function App() {
-  return <h1>Hello Rocketseat</h1>
+  return (
+    <>
+    <h1>Hello Rocketseat!</h1>
+    <img width="250px" src={profile} />
+    </>
+    );
+
 }
 
 export default App;
