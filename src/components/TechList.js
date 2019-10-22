56d391171a2b71f0136f8d6ab0094837c31a4a50
gusflopes <gusflopes86@gmail.com>
2019-10-22 15:00:07 -0400

    Basics of state/setState

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: [
       'Node.js', 'ReactJS', 'React Native'
     ]
   };
 
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
 
   render() {
-    console.log(this.state);
-    
+
     return (
       <>
+      <h2>{this.state.newTech}</h2>
       <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
+        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
       </ul>
+      <input type="text" onChange={this.handleInputChange}/>
+      <button type="submit">+</button>
       </>
     )
   }
